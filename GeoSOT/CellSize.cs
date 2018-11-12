8b2093b92ebe929b488febbba913a20d9054fc87
zhengshl <zhengsl@satimage.cn>
2018-11-12 15:46:37 +0800

    init version

---
+++ GeoSOT/CellSize.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GeoSOT
+{
+    public class CellSize
+    {
+        /// <summary>
+        /// 获取level级别的分块大小,单位为经纬度
+        /// </summary>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        public double GetCellSize(int level)
+        {
+            double cellSize = 0;
+            if (level >= 0 && level <= 9)
+            {
+                cellSize = Math.Pow(2, 9 - level);
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+            return cellSize;
+        }
+    }
+}
